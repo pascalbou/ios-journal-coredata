3aba5c21b07996a5d9d9a614dd0165dd7616391c
krikaz <pascal.bou@protonmail.com>
2020-05-04 23:06:19 +0200

    part3 create entry VC

--- Journal/Journal/View Controllers/CreateEntryViewController.swift
+++ Journal/Journal/View Controllers/CreateEntryViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class CreateEntryViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        navigationController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func save(_ sender: UIBarButtonItem) {
+        guard let title = titleTextField.text, !title.isEmpty else { return }
+        
+        let bodyText = bodyTextView.text
+        Entry(title: title, bodyText: bodyText)
+        do {
+            try CoreDataStack.shared.mainContext.save()
+        } catch {
+            NSLog("Error saving managed object context: \(error)")
+            return
+        }
+        
+        navigationController?.dismiss(animated: true, completion: nil)
+    }
 
 }
 
