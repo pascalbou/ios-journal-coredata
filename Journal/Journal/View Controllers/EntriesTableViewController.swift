f7c1faff18d4b7d44902072acf4ad6cb19400132
krikaz <pascal.bou@protonmail.com>
2020-05-06 19:40:10 +0200

    part 2

--- Journal/Journal/View Controllers/EntriesTableViewController.swift
+++ Journal/Journal/View Controllers/EntriesTableViewController.swift
@@ -11,20 +11,15 @@ import CoreData
 
 class EntriesTableViewController: UITableViewController {
     
-    // This is not a good, efficient way to do this, as the fetch request
-    // will be executed every time the tasks property is accessed. We will
-    // learn a better way to do this later
-    
-    var entries: [Entry] {
+    lazy var fetchedResultsController: NSFetchedResultsController<Entry> = {
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "mood", ascending: true), NSSortDescriptor(key: "timestamp", ascending: true)]
         let context = CoreDataStack.shared.mainContext
-        do {
-            return try context.fetch(fetchRequest)
-        } catch {
-            NSLog("Error fetching tasks: \(error)")
-            return []
-        }
-    }
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: "mood", cacheName: nil)
+        frc.delegate = self
+        try! frc.performFetch()
+        return frc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,9 +34,12 @@ class EntriesTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return entries.count
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
 
     
@@ -50,43 +48,79 @@ class EntriesTableViewController: UITableViewController {
             fatalError("Can't dequeue cell of type \(EntryTableViewCell.reuseIdentifier)")
         }
 
-        cell.entry = entries[indexPath.row]
+        cell.entry = fetchedResultsController.object(at: indexPath)
 
         return cell
     }
     
 
-    // Override to support editing the table view.
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            // Delete the row from the data source
-//            //tableView.deleteRows(at: [indexPath], with: .fade)
-//            tableView.delete(indexPath.row)
-//
-//            CoreDataStack.shared.container.viewContext.delete(entries[indexPath.row])
-//
-//            do {
-//                try CoreDataStack.shared.mainContext.save()
-//            } catch {
-//                NSLog("Error saving managed object context: \(error)")
-//                return
-//            }
-//
-//        } else if editingStyle == .insert {
-//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//        }
-//    }
+//     Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            let entry = fetchedResultsController.object(at: indexPath)
+            CoreDataStack.shared.mainContext.delete(entry)
+
+            do {
+                try CoreDataStack.shared.mainContext.save()
+            } catch {
+                CoreDataStack.shared.mainContext.reset()
+                NSLog("Error saving managed object context: \(error)")
+            }
+        }
+    }
     
 
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "PresentCreateEntrySegue" {
+//            if let detailVC = segue.destination as? CreateEntryViewController, let indexPath = tableView.indexPathForSelectedRow {
+//                detailVC.entry = fetchedResultsController.object(at: indexPath)
+//            }
+//        }
+//    }
 
+}
+
+extension EntriesTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath, let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
+    }
 }
